48310999fcca72d164eab8319bc62a2023ad900e
Leo van Hengel <leo.van.hengel@gmail.com>
2021-12-12 19:10:24 +0100

    change index.ts to unique name for each day

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,27 @@ if (!Deno.args.length) {
 }
 
 const day = Deno.args[0];
-const file = Deno.args[1];
+//const input = Deno.args[1];
 
-const currentDay = await import(`./day${day}/index.ts`);
+//const currentDay = await import(`./day${day}/index.ts`);
 
 // Deno.readTextFile needs path to file from where script is started
-const input = await Deno.readTextFile(`./src/day${day}/${file}`);
+const input = await Deno.readTextFile(`./src/day${day}/${Deno.args[1]}`);
 
-currentDay.part1(input);
-currentDay.part2(input);
+let file;
+for await (const dirEntry of Deno.readDir(`./src/day${day}/`)) {
+  if (dirEntry.isFile && dirEntry.name.endsWith(".ts")) {
+    file = dirEntry.name;
+  }
+}
+
+if (file) {
+  const js = await import(`./day${day}/${file}`);
+  js.part1(input);
+  js.part2(input);
+}
+
+//console.log(dir);
+
+//currentDay.part1(input);
+//currentDay.part2(input);
